dacbb3ce98d15f05a051bf2193df318624a1fdd8
Ravy Lim <ravy.lim@truemoney.com.kh>
2018-08-14 10:00:56 +0700

    commit

---
+++ limravylib/src/main/java/com/androidlover/limravylib/Limravy.java
@@ -0,0 +1,12 @@
+package com.androidlover.limravylib;
+
+import android.util.Log;
+
+public class Limravy {
+
+    public static void ravy(){
+        Log.d("ravy","Congratulation!! You are using limravylib library ");
+    }
+
+
+}
